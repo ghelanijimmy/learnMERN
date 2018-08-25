099b461810dbf056962de37ddc3a9bd37eef1d77
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2018-08-25 00:19:09 -0400

    login and register routes
    set up login and regiser routes and test them in postman for logic along with password hashing with bcrypt

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
@@ -16,7 +21,7 @@ mongoose
   .then(()=>console.log('MongoDB Connected'))
   .catch(error=>console.log(error));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 //USE routes
 app.use('/api/users', users);
