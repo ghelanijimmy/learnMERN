d9ad6a02a99966683c0a1f8ed9a21d9c5443ceb2
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2018-09-01 12:50:56 -0400

    auth tokens

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import {GET_ERRORS} from './types';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // Register User
 export const registeruser = (userData, history) => dispatch => {
@@ -7,3 +9,30 @@ export const registeruser = (userData, history) => dispatch => {
   .then(res => history.push('/login'))
   .catch(err => dispatch({type: GET_ERRORS, payload: err.response.data}));
 };
+
+
+// Login - get user token
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // Save to local storage
+      const {token} = res.data;
+      // Set token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to auth header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => dispatch({type: GET_ERRORS, payload: err.response.data}));
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import classnames from 'classnames';
+import {loginUser} from '../../actions/authActions';
 
 class Login extends Component {
 
@@ -13,20 +17,33 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+
+    if(nextProps.auth.isAuthenticated){
+      this.props.history.push('/dashboard');
+    }
+
+    if(nextProps.errors){
+      this.setState({errors: nextProps.errors})
+    }
+  }
+
   onChange(e) {
     this.setState({[e.target.name]: e.target.value});
   }
 
   onSubmit(e){
     e.preventDefault();
-    const newUser = {
+    const userData = {
       email: this.state.email,
       password: this.state.password
     }
-    console.log(newUser);
+    this.props.loginUser(userData);
   }
 
   render() {
+    const {errors} = this.state;
+
     return(
       <div className="login">
         <div className="container">
@@ -40,18 +57,24 @@ class Login extends Component {
                   value={this.state.email}
                   onChange={this.onChange}
                   type="email"
-                  className="form-control form-control-lg"
+                  className={classnames('form-control form-control-lg', {
+                    'is-invalid': errors.email
+                  })}
                   placeholder="Email Address"
                   name="email" />
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                 </div>
                 <div className="form-group">
                   <input
                   value={this.state.password}
                   onChange={this.onChange}
                   type="password"
-                  className="form-control form-control-lg"
+                  className={classnames('form-control form-control-lg', {
+                    'is-invalid': errors.password
+                  })}
                   placeholder="Password"
                   name="password" />
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -63,4 +86,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, {loginUser})(Login);
